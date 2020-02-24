621d7103bb8ea61a6ab2b6b875ebe421bc333d10
peterkang2001 <peterkang2001@gmail.com>
2020-02-24 15:01:44 +0800

    添加广告页面

---
+++ app/src/main/java/cn/peterkang/krishna/Advertisement_1.java
@@ -0,0 +1,14 @@
+package cn.peterkang.krishna;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Advertisement_1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_advertisement_1);
+    }
+}
